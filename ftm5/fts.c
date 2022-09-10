tbalden/google-modules-fts_touch

commit d307b1ba0326b4ffe0e28b9c7f7721b585ff9b9f

ftm5: enable DMA mode
Enable DMA mode and set the data chunk to maximum.

Bug: 244660551
Test: Run all FATP commands. Do the firmware update.

Signed-off-by: davidycchen <davidycchen@google.com>
Change-Id: I339a3c801aac6b17d394673a2ea15af94db06606

ftm5/fts_lib/ftsIO.c
@@ -126,7 +126,8 @@ static int fts_read_internal(struct fts_ts_info *info, u8 *outBuf,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = byteToRead;
+	transfer[0].len = spi_len_dma_align(byteToRead, 4);
+	transfer[0].bits_per_word = spi_bits_dma_align(byteToRead);
 	transfer[0].delay.value = SPI_DELAY_CS;
 	transfer[0].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[0].tx_buf = NULL;
@@ -214,12 +215,14 @@ static int fts_writeRead_internal(struct fts_ts_info *info, u8 *cmd,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = cmdLength;
+	transfer[0].len = spi_len_dma_align(cmdLength, 4);
+	transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
 	transfer[0].tx_buf = cmd;
 	transfer[0].rx_buf = NULL;
 	spi_message_add_tail(&transfer[0], &msg);
 
-	transfer[1].len = byteToRead;
+	transfer[1].len = spi_len_dma_align(byteToRead, 4);
+	transfer[1].bits_per_word = spi_bits_dma_align(byteToRead);
 	transfer[1].delay.value = SPI_DELAY_CS;
 	transfer[1].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[1].tx_buf = NULL;
@@ -293,7 +296,8 @@ static int fts_write_internal(struct fts_ts_info *info, u8 *cmd, int cmdLength,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = cmdLength;
+	transfer[0].len = spi_len_dma_align(cmdLength, 4);
+	transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
 	transfer[0].delay.value = SPI_DELAY_CS;
 	transfer[0].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[0].tx_buf = cmd;
@@ -389,17 +393,20 @@ static int fts_writeThenWriteRead_internal(struct fts_ts_info *info,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = writeCmdLength;
+	transfer[0].len = spi_len_dma_align(writeCmdLength, 4);
+	transfer[0].bits_per_word = spi_bits_dma_align(writeCmdLength);
 	transfer[0].tx_buf = writeCmd1;
 	transfer[0].rx_buf = NULL;
 	spi_message_add_tail(&transfer[0], &msg);
 
-	transfer[1].len = readCmdLength;
+	transfer[1].len = spi_len_dma_align(readCmdLength, 4);
+	transfer[1].bits_per_word = spi_bits_dma_align(readCmdLength);
 	transfer[1].tx_buf = readCmd1;
 	transfer[1].rx_buf = NULL;
 	spi_message_add_tail(&transfer[1], &msg);
 
-	transfer[2].len = byteToRead;
+	transfer[2].len = spi_len_dma_align(byteToRead, 4);
+	transfer[2].bits_per_word = spi_bits_dma_align(byteToRead);
 	transfer[2].delay.value = SPI_DELAY_CS;
 	transfer[2].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[2].tx_buf = NULL;

ftm5/fts_lib/ftsHardware.h
@@ -41,14 +41,15 @@
 #define DCHIP_FW_VER_BYTE		2	/* /< number of bytes of the fw
 						 * versions */
 
-/* CHUNKS */
-#define READ_CHUNK			1024	/* /< chunk dimension of
+/* CHUNKS. It can support up to 2048 bytes. Set as 2040 to make sure it still less
+   than 2048 after adding byte_to_skip and 4 bytes alignment. */
+#define READ_CHUNK			2040	/* /< chunk dimension of
 						 * a single i2c read,
 						 * max allowed value is 2kB */
-#define WRITE_CHUNK			1024	/* /< chunk dimension of
+#define WRITE_CHUNK			2040	/* /< chunk dimension of
 						 * a single i2c write,
 						 * max allowed value is 2kB */
-#define MEMORY_CHUNK			1024	/* /< chunk dimenasion of
+#define MEMORY_CHUNK			2040	/* /< chunk dimenasion of
 						 * a single i2c write on mem,
 						 * max allowed value is 2kB */
 

ftm5/fts_lib/ftsIO.h
@@ -35,6 +35,8 @@
 #include <linux/spi/spi.h>
 #endif
 
+#define spi_len_dma_align(len, sz) ((len) >= 64) ? ALIGN(len, sz) : (len)
+#define spi_bits_dma_align(len) ((len) >= 64) ? (32) : (8)
 
 int openChannel(void *clt);
 

commit 65009f1d33086f231d8a5fbf2081e26a87a02f42

ftm5: fix reads/writes when dma mode isn't supported
When dma mode isn't support or enabled, we need to fallback to the
regular reads/writes. Add a dma_mode check to know if it's enabled or
disabled. Oriole in particular doesn't enable dma mode which causes
touch to fail without this patch.

Bug: 246007821
Test: verify touch on oriole
Fixes: d307b1ba0326 ("ftm5: enable DMA mode")
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: I7cc9290a347f5724a3b709b511b362436ea2c2ab

ftm5/fts.c
@@ -75,6 +75,8 @@
 #include "fts_lib/ftsTime.h"
 #include "fts_lib/ftsTool.h"
 
+#include <goog_touch_interface.h>
+
 /* Touch simulation MT slot */
 #define TOUCHSIM_SLOT_ID		0
 #define TOUCHSIM_TIMER_INTERVAL_NS	8333333
@@ -6491,12 +6493,20 @@ static int fts_probe(struct spi_device *client)
 	dev_info(&client->dev, "driver ver. %s\n", FTS_TS_DRV_VERSION);
 
 	dev_info(&client->dev, "SET Bus Functionality :\n");
+
+	info = kzalloc(sizeof(struct fts_ts_info), GFP_KERNEL);
+	if (!info) {
+		dev_err(&client->dev, "Out of memory... Impossible to allocate struct info!\n");
+		error = -ENOMEM;
+		goto ProbeErrorExit_0;
+	}
+
 #ifdef I2C_INTERFACE
 	dev_info(&client->dev, "I2C interface...\n");
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		dev_err(&client->dev, "Unsupported I2C functionality\n");
 		error = -EIO;
-		goto ProbeErrorExit_0;
+		goto ProbeErrorExit_1;
 	}
 
 	dev_info(&client->dev, "i2c address: %x\n", client->addr);
@@ -6509,18 +6519,13 @@ static int fts_probe(struct spi_device *client)
 			dev_err(&client->dev, "%s: setup SPI rt failed(%d)\n",
 				__func__, retval);
 		}
+
+		info->dma_mode = goog_check_spi_dma_enabled(client);
 	}
 	dev_info(&client->dev, "SPI interface...\n");
 	bus_type = BUS_SPI;
 #endif
 	dev_info(&client->dev, "SET Device driver INFO:\n");
-
-	info = kzalloc(sizeof(struct fts_ts_info), GFP_KERNEL);
-	if (!info) {
-		dev_err(&client->dev, "Out of memory... Impossible to allocate struct info!\n");
-		error = -ENOMEM;
-		goto ProbeErrorExit_0;
-	}
 
 	info->client = client;
 	info->dev = &info->client->dev;

ftm5/fts.h
@@ -989,7 +989,7 @@ struct fts_ts_info {
 	u8 io_write_buf[WRITE_CHUNK + BITS_64 + DUMMY_FIFO];
 	/* Preallocated i/o extra write buffer */
 	u8 io_extra_write_buf[WRITE_CHUNK + BITS_64 + DUMMY_FIFO];
-
+	bool dma_mode;
 };
 
 /* DSI display function used to read panel extinfo */

ftm5/fts_lib/ftsIO.c
@@ -126,8 +126,12 @@ static int fts_read_internal(struct fts_ts_info *info, u8 *outBuf,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = spi_len_dma_align(byteToRead, 4);
-	transfer[0].bits_per_word = spi_bits_dma_align(byteToRead);
+	if (info->dma_mode) {
+		transfer[0].len = spi_len_dma_align(byteToRead, 4);
+		transfer[0].bits_per_word = spi_bits_dma_align(byteToRead);
+	} else {
+		transfer[0].len = byteToRead;
+	}
 	transfer[0].delay.value = SPI_DELAY_CS;
 	transfer[0].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[0].tx_buf = NULL;
@@ -215,14 +219,22 @@ static int fts_writeRead_internal(struct fts_ts_info *info, u8 *cmd,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = spi_len_dma_align(cmdLength, 4);
-	transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
+	if (info->dma_mode) {
+		transfer[0].len = spi_len_dma_align(cmdLength, 4);
+		transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
+	} else {
+		transfer[0].len = cmdLength;
+	}
 	transfer[0].tx_buf = cmd;
 	transfer[0].rx_buf = NULL;
 	spi_message_add_tail(&transfer[0], &msg);
 
-	transfer[1].len = spi_len_dma_align(byteToRead, 4);
-	transfer[1].bits_per_word = spi_bits_dma_align(byteToRead);
+	if (info->dma_mode) {
+		transfer[1].len = spi_len_dma_align(byteToRead, 4);
+		transfer[1].bits_per_word = spi_bits_dma_align(byteToRead);
+	} else {
+		transfer[1].len = byteToRead;
+	}
 	transfer[1].delay.value = SPI_DELAY_CS;
 	transfer[1].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[1].tx_buf = NULL;
@@ -296,8 +308,12 @@ static int fts_write_internal(struct fts_ts_info *info, u8 *cmd, int cmdLength,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = spi_len_dma_align(cmdLength, 4);
-	transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
+	if (info->dma_mode) {
+		transfer[0].len = spi_len_dma_align(cmdLength, 4);
+		transfer[0].bits_per_word = spi_bits_dma_align(cmdLength);
+	} else {
+		transfer[0].len = cmdLength;
+	}
 	transfer[0].delay.value = SPI_DELAY_CS;
 	transfer[0].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[0].tx_buf = cmd;
@@ -393,20 +409,32 @@ static int fts_writeThenWriteRead_internal(struct fts_ts_info *info,
 #else
 	spi_message_init(&msg);
 
-	transfer[0].len = spi_len_dma_align(writeCmdLength, 4);
-	transfer[0].bits_per_word = spi_bits_dma_align(writeCmdLength);
+	if (info->dma_mode) {
+		transfer[0].len = spi_len_dma_align(writeCmdLength, 4);
+		transfer[0].bits_per_word = spi_bits_dma_align(writeCmdLength);
+	} else {
+		transfer[0].len = writeCmdLength;
+	}
 	transfer[0].tx_buf = writeCmd1;
 	transfer[0].rx_buf = NULL;
 	spi_message_add_tail(&transfer[0], &msg);
 
-	transfer[1].len = spi_len_dma_align(readCmdLength, 4);
-	transfer[1].bits_per_word = spi_bits_dma_align(readCmdLength);
+	if (info->dma_mode) {
+		transfer[1].len = spi_len_dma_align(readCmdLength, 4);
+		transfer[1].bits_per_word = spi_bits_dma_align(readCmdLength);
+	} else {
+		transfer[1].len = readCmdLength;
+	}
 	transfer[1].tx_buf = readCmd1;
 	transfer[1].rx_buf = NULL;
 	spi_message_add_tail(&transfer[1], &msg);
 
-	transfer[2].len = spi_len_dma_align(byteToRead, 4);
-	transfer[2].bits_per_word = spi_bits_dma_align(byteToRead);
+	if (info->dma_mode) {
+		transfer[2].len = spi_len_dma_align(byteToRead, 4);
+		transfer[2].bits_per_word = spi_bits_dma_align(byteToRead);
+	} else {
+		transfer[2].len = byteToRead;
+	}
 	transfer[2].delay.value = SPI_DELAY_CS;
 	transfer[2].delay.unit = SPI_DELAY_UNIT_USECS;
 	transfer[2].tx_buf = NULL;